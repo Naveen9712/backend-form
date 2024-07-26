24f8c40dbdb0a47d0d6c9aff8ace8a1c35aeb3e2
Naveen9712 <kandakurthinaveen97@gmail.com>
2024-07-26 16:48:06 +0530

    u

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require('mysql');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const dbConfig = require('./dbConfig'); // Import the database configuration
 
 const app = express();
